082f989197e5626f3eb78e0bc378b45f7072b077
aspa1 <aspa.karana@gmail.com>
2015-11-16 23:03:15 +0200

    fixed imread problem

--- src/svm.cpp
+++ src/svm.cpp
@@ -24,30 +24,37 @@ bool SVM::image_callback ( 	svm_project::trainSvmSrv::Request  &req,
 	
 	if (boost::filesystem::is_directory(p))
 	{
-		//cv::Mat image;
+		cv::Mat image;
 		ROS_INFO_STREAM("Positives:");
 		for (boost::filesystem::directory_iterator itr(p); itr!=boost::filesystem::directory_iterator(); ++itr)
 		{
 			ROS_INFO_STREAM("Name:" << itr->path().filename()); // display filename only
 			std::string p1 =(itr->path().filename()).string();
 			//ROS_INFO_STREAM("Image name:" << p1);
-			std::string p_= p + p1;
-			//ROS_INFO_STREAM("Full name:" << p_);
-			
-			
-			//image = cv::imread(p_ , CV_LOAD_IMAGE_COLOR);
-			//ROS_INFO_STREAM (image.rows);
+			std::string p_= p + "/" + p1;
+			ROS_INFO_STREAM("Full name:" << p_);
+			image = cv::imread(p_ , CV_LOAD_IMAGE_COLOR);
+			ROS_INFO_STREAM (image.rows);
 			//cv::imshow( "Display window", image );
-			//cv::waitKey(5);
+			cv::waitKey(5);
 		}
 	}
 	
 	if (boost::filesystem::is_directory(n))
 	{
+		cv::Mat image;
 		ROS_INFO_STREAM("Negatives:");
 		for (boost::filesystem::directory_iterator itr(n); itr!=boost::filesystem::directory_iterator(); ++itr)
 		{
 			ROS_INFO_STREAM("Name:" << itr->path().filename()); // display filename only
+			std::string n1 =(itr->path().filename()).string();
+			//ROS_INFO_STREAM("Image name:" << n1);
+			std::string n_= n + "/" + n1;
+			ROS_INFO_STREAM("Full name:" << n_);
+			image = cv::imread(n_ , CV_LOAD_IMAGE_COLOR);
+			ROS_INFO_STREAM (image.rows);
+			//cv::imshow( "Display window", image );
+			cv::waitKey(5);
 		}
 	}
 	res.success = true;
