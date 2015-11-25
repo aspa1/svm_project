33cd9ddfc5373075108f457da79fa4cccf45048e
aspa1 <aspa.karana@gmail.com>
2015-11-25 21:36:21 +0200

    fixed script problem

--- scripts/call_svm_service.py
+++ scripts/call_svm_service.py
@@ -7,17 +7,17 @@ import sys
 
 #~ p = '/samples/positives' 				#positive_samples directory
 #~ n = '/samples/negatives'				#negative_samples directory
-#~ name='/out.jpg'							#name of the image to be tested 
+#~ name='/out.jpg'						#name of the image to be tested 
 
 def call_image_receiver_server():
     rospy.wait_for_service('image_receiver')
     try:
         service_handle = rospy.ServiceProxy('image_receiver', trainSvmSrv)
         resp1 = service_handle(p, n)
-        print "Image receiver service called"
+        print "Training SVM model service called"
         return resp1.success
     except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+        print "Training SVM model service call failed: %s"%e
         return resp1.success
         
 
@@ -25,28 +25,26 @@ def call_uri_retriever_server():
     rospy.wait_for_service('uri_retriever')
     try:
         service_handle2 = rospy.ServiceProxy('uri_retriever', urlRetrieverSrv)
-        resp2 = service_handle2(imagePath)
+        resp2 = service_handle2(name)
         print "Uri retriever service called"
         return resp2.success
     except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+        print "Uri retriever service call failed: %s"%e
         return resp2.success
 
+
 def usage():
-	#~ print "Requesting the folder path for positives,negatives and an image uri"
-    return "%s [p n imagePath]"%sys.argv[0]
-    #~ return "%s [x y]"%sys.argv[0]
-     
+	#~ print "jhjh"
+    return "%s [Requesting paths for positivesFolder negativesFolder imageToBeTested]"%sys.argv[0]
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
         p = str(sys.argv[1])
         n = str(sys.argv[2])
-        imagePath= str(sys.argv[3])
+        name = str(sys.argv[3])
     else:
         print usage()
         sys.exit(1)
-        
-	print call_image_receiver_server()
-	print call_uri_retriever_server()      
-	
+    #~ print "Requesting pos neg name"
+    print call_image_receiver_server()
+    print call_uri_retriever_server()
