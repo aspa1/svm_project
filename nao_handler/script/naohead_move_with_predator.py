49082af1fdc97a427126ec758650138ff29e6458
chrisagou <chrisagouniotou@gmail.com>
2016-03-29 19:33:45 +0300

    changes in naohead_move_with_predator

--- nao_handler/script/naohead_move_with_predator.py
+++ nao_handler/script/naohead_move_with_predator.py
@@ -12,24 +12,29 @@ class move_nao_head:
 	def __init__(self):
 		s = rospy.Service('/body_stiffness/enable', Empty, self.call_body_stifness_server)
 		rospy.Subscriber("/joint_angles", JointAnglesWithSpeed, self.move)
-		pub = rospy.Publisher('/joint_angles', JointAnglesWithSpeed, queue_size=10)
+		self.pub = rospy.Publisher('/joint_angles', JointAnglesWithSpeed, queue_size=10)
 		print "hello31"
 	
 	def move(self):
+		rospy.init_node('nao_head', anonymous=True)
+		rate = rospy.Rate(10) # 10hz
 		while not rospy.is_shutdown():
 			joint= JointAnglesWithSpeed()
 			pred_point= Polygon() 
-			joint.joint_names[0]= "HeadYaw"
+			#~ joint.joint_names.append("HeadYaw")
 			#~ joint.joint_names[1]= "HeadPitch"
-			for i in range(0,1):	
+			#~ joint.joint_angles= 0*len(pred_point.points)
+			for i in range(len(pred_point.points)-1):
+				print "hello41"	
 				joint.joint_angles[i]= pred_point.points[i]
+				print joint.joint_angles[i]
 				#~ joint.joint_angles[2]= pred_point.points[2]
 			joint.speed= 0.1
 			
 			#~ hello_str = "hello world %s" % rospy.get_time()
 			#~ rospy.loginfo(hello_str)
-			for i in range(0,1):
-				pub.publish(joint.joint_angles[i])
+			for i in range(len(pred_point.points)-1):
+				self.pub.publish(joint.joint_angles[i])
 			
 	def call_body_stifness_server(self):
 		print "hello1"
