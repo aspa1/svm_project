50a82b936e70923ed4dd0544dea0a615699ba959
chrisagou <chrisagouniotou@gmail.com>
2016-05-11 13:39:54 +0300

    path planning

--- nao_handler/scripts/move_head_and_body_with_predator.py
+++ nao_handler/scripts/move_head_and_body_with_predator.py
@@ -5,10 +5,13 @@ from geometry_msgs.msg import Twist
 from std_srvs.srv import Empty
 from naoqi_bridge_msgs.msg import JointAnglesWithSpeed
 from sensor_msgs.msg import CameraInfo
+from nav_msgs.msg import Path
+from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped
+from visualization_msgs.msg import Marker
 from rapp_robot_api import RappRobot 
-from nao_handler.srv import SetBehavior
-#~ from nao_handler.srv import SetGoal
-#~ from localization_project.h import particle
+from nao_handler.srv import GetPath
+from ogmpp_communications.srv import OgmppPathPlanningSrv
 
 import rospy
 import sys
@@ -19,8 +22,10 @@ class MoveHeadAndBody:
 		self.rh = RappRobot()
 		self.pub = rospy.Publisher('/joint_angles', JointAnglesWithSpeed, queue_size=1)
 		self.publ = rospy.Publisher('/inner/cmd_vel', Twist, queue_size=1)
+		self.path_publisher = rospy.Publisher('/as/path', \
+			Path, queue_size = 10)
 		self.s = rospy.Service('set_behavior', SetBehavior, self.set_behavior)
-		#~ self.sub = rospy.Service('set_goal', SetGoal, self.set_goal)
+		self.service_path = rospy.Service('get_path', GetPath, self.get_path)
 		self.rh.motion.enableMotors()
 		self.rh.humanoid_motion.goToPosture("Stand", 0.7)
 		self.lost_object_counter = 20
@@ -29,8 +34,12 @@ class MoveHeadAndBody:
 		self.x_vel = 0
 		self.y_vel = 0
 		self.theta_vel = 0
+		self.robot_x = 0
+		self.robot_y = 0
+		self.path = []
 		rospy.Timer(rospy.Duration(0.1), self.lost_object_callback)
 		rospy.Timer(rospy.Duration(0.1), self.set_velocities_callback)
+		rospy.Timer(rospy.Duration(0.1), self.get_robot_position_callback)
 	
 	def track_bounding_box(self, polygon):
 		self.hunt_initiated = True
@@ -112,9 +121,7 @@ class MoveHeadAndBody:
 	def set_velocities_callback(self, event):
 		
 		self.rh.motion.moveByVelocity(self.x_vel, self.y_vel, self.theta_vel)
-		#~ rospy.loginfo("%s", self.x_vel)
-		#~ rospy.loginfo("%s", self.y_vel)
-		#~ rospy.loginfo("%s", self.theta_vel)
+		
 		
 		velocities = Twist()
 		
@@ -161,9 +168,43 @@ class MoveHeadAndBody:
 			res = SetBehavior()
 			res.success = True
 			return True
-	
-	#~ def set_goal(self, request):
+	rospy
+	def get_path(self, request):
+		rospy.wait_for_service('/ogmpp_path_planners/plan')
+		try:
+			path = rospy.ServiceProxy('/ogmpp_path_planners/plan', OgmppPathPlanningSrv)
+			path.method = "uniform_prm"
+			path.data.begin.x = self.robot_x
+			path.data.begin.y = self.robot_y
+			path.data.end.x = request.x
+			path.data.end.y = request.y
+			
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
 		
+		ros_path = Path()
+		
+        #~ ros_path.header.frame_id = "map"
+		_map = OccupancyGrid()
+        
+		for p in self.path:
+			ps = PoseStamped()
+			ps.header.frame_id = "map"
+			ps.pose.position.x = p[0] * _map.info.resolution + \
+				_map.info.origin['x']
+			ps.pose.position.y = p[1] * _map.info.resolution + \
+				_map.info.origin['y']
+			
+			ros_path.poses.append(ps)
+		
+		self.path_publisher.publish(ros_path)
+		
+				
+	def get_robot_position_callback(self, event):
+		robot = Marker()
+		if robot.ns == "Best Particle":
+			self.robot_x = robot.points.x
+			self.robot_y = robot.points.y
 			
 		
 if __name__ == "__main__":
